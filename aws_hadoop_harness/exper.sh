b60495a61a90e0d98ba7b82eef1cd6abf2fc2ea5
Shivnath Babu <shivnath@kuku.(none)>
2010-07-15 09:46:39 -0400

    First step towards converting the current harness which supports only terasort into a general-purpose harness for running arbitrary MapReduce programs written as jars. See the newly added config.8.xml sample file to get an idea of how to specify the input set of experiments/jobs to run

--- aws_hadoop_harness/exper.sh
+++ aws_hadoop_harness/exper.sh
@@ -1,131 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script will loop over a set of directories where preprocessing
 #   code has placed Hadoop configuration files for each experiment, 
-#   and run a customized command (from the bash command line) to run the experiment
-
+#   and run a customized bash script to run the experiment
+#
 # Usage: Only one parameter. 
-# Parameter #1 is the file listing THE FULL PATH (not relative path) to all experiment directories
-
-# NOTE: This script runs hadoop command like the following: 
-# ${HADOOP_HOME}/bin/hadoop jar ${HADOOP_HOME}/hadoop-*-examples.jar terasort -conf configuration.xml /user/shivnath/tera/in /user/shivnath/tera/out
-# NOTE that the "-conf" format is used to specify a Hadoop configuration file in the 
-#  command line. This format will work only if the main Java class that is run implements
-#  the Tool interface. 
+# Parameter #1 is the file listing the path to all experiment directories
 
 ##########################################################################
 
-# Variables that will need to be adjusted to ensure that the appropriate 
-#  command gets generated to run the experiment on Hadoop
-
-# The full path to the hadoop binary in the hadoop bin directory
-declare HADOOP_BIN="${HADOOP_HOME}/bin/hadoop"
-
-# The specification of the jar to run as part of the hadoop command
-declare JAR_SPECIFICATION="jar ${HADOOP_HOME}/hadoop-*-examples.jar terasort"
-
-# The input directory or file for each experiment 
-declare HDFS_INPUT_DIR="/user/shivnath/tera/in"
-
-# The output directory for each experiment. This will be removed before each experiment
-#   since hadoop needs a new (not created) directory per experiment
-declare HDFS_OUTPUT_DIR="/user/shivnath/tera/out"
-
-##########################################################################
-
-# Variables below should not need to be changed
-
-# The name of the file (in each experiment directory) specifying the parameter configuration.
+# The name of the file (in each experiment directory) specifying the parameter configuration
+#   for the MapReduce job that will run the experiment.
 #   The preprocessing code creates this file per experiment/directory, so be careful
-#   if you want to change this variable (that means you will need to change the preprocessing
-#   code as well)
-declare XML_CONFIGURATION_FILE="configuration.xml"
+#   if you change this variable (the you need to change the preprocessing code as well)
+declare XML_CONF_FILE_NAME="configuration.xml"
 
-# The output of the hadoop command is written to this file (in each experiment directory)
-declare EXPERIMENT_OUTPUT_FILE="output.txt"
+# The name of the file (in each experiment directory) specifying the 
+#  name of the hadoop command script that will be used to run the experiment
+declare HADOOP_JAR_COMMAND_SCRIPT="run_hadoop_jar.sh"
 
 ##########################################################################
 
 #Check Usage
 if [ $# -ne 1 ] ; then
-    printf "Usage: Parameter #1 is the file listing THE FULL PATH (not relative path) to all directories\n"
-    exit 0
+    printf "Usage: Parameter #1 is the file listing the path to all experiment directories\n"
+    exit -1
 fi
+
+# the file listing all input directories (that will each be read later)
+declare INPUT_DIR_FILE=$1
+if test ! -e $INPUT_DIR_FILE; then
+    printf "ERROR: Invalid file listing all input experiment directories. exper.sh is exiting\n"
+    exit -1
+fi
+
+# the loop that goes into each experiment directory and runs the experiment follows 
 
 declare CURR_DIR=`pwd`
-
-# Check whether parameters have been specified correctly
-
-if test ! -e $HADOOP_BIN; then
-    printf "Specified Hadoop binary (hadoop) does not exist\n"
-    printf "Exiting\n"
-    exit 0
-fi
-
-if test ! -x $HADOOP_BIN; then
-    printf "Specified Hadoop binary (hadoop) is not executable\n"
-    printf "Exiting\n"
-    exit 0
-fi
-
-# the file listing all input directories (that will be read later)
-declare INPUT_DIR_FILE=$1
-
-if test ! -e $INPUT_DIR_FILE; then
-    printf "Invalid file listing all input directories\n"
-    printf "Exiting\n"
-    exit 0
-fi
-
-##################################################################
-##################################################################
-
-# the loop that creates the environment and runs each experiment
-
 declare direc
-declare hadoop_command
-
 exec 3< $INPUT_DIR_FILE
 
 while read direc <&3 ; do
-
-   printf "Entering experiment input directory %s\n" "$direc"
-
-   cd $direc
-
-   if test ! -e $XML_CONFIGURATION_FILE; then
-      printf "Hadoop configuration file %s does not exist in the experiment directory %s\n" "$XML_CONFIGURATION_FILE" "$direc"
-      printf "Skipping this experiment\n"
-      continue
+    
+    # this step ensures that the program will work irrespective of whether 
+    #   the paths specified in INPUT_DIR_FILE are absolute or relative paths 
+    cd $CURR_DIR
+    
+    printf "Entering experiment input directory %s\n" "$direc"
+    cd $direc
+    
+   if test ! -e $XML_CONF_FILE_NAME; then
+       printf "Hadoop configuration file %s does not exist in the experiment directory %s\n" "$XML_CONF_FILE_NAME" "$direc"
+       printf "Skipping this experiment\n"
+       printf "Exiting experiment input directory %s\n" "$direc"
+       continue
    fi
-
-   # Remove the output directory if it exists
-   # example: /vol/local/hadoop1/hadoop-0.20.1/bin/hadoop fs -rmr /user/shivnath/tera/out"
-   hadoop_command="$HADOOP_BIN fs -rmr $HDFS_OUTPUT_DIR"
-
-   printf "Going to run the command: %s\n" "$hadoop_command"
-
-   $hadoop_command
-
-   printf "Finished running the command: %s\n" "$hadoop_command"
-
-   # Run the experiment
-   # example: /vol/local/hadoop1/hadoop-0.20.1/bin/hadoop jar /vol/local/hadoop1/hadoop-0.20.1/hadoop-0.20.1-examples.jar terasort -conf configuration.xml /user/shivnath/tera/in /user/shivnath/tera/out
-
-   hadoop_command="$HADOOP_BIN $JAR_SPECIFICATION -conf $XML_CONFIGURATION_FILE $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR" 
-   printf "Going to run the command: %s\n" "$hadoop_command"
-
-   $hadoop_command >& $direc/$EXPERIMENT_OUTPUT_FILE &
-
-   # we have to wait here until the above command terminates
-   wait $!  #wait for the last background process to exit
-
-   printf "Finished running the command: %s\n" "$hadoop_command"
+   
+   if test ! -x $HADOOP_JAR_COMMAND_SCRIPT; then
+       printf "Script %s does not exist or is not executable in the experiment directory %s\n" "$HADOOP_JAR_COMMAND_SCRIPT" "$direc"
+       printf "Skipping this experiment\n"
+       printf "Exiting experiment input directory %s\n" "$direc"
+       continue
+   fi
+   
+   # run the script 
+   . $HADOOP_JAR_COMMAND_SCRIPT
+   
    printf "Exiting experiment input directory %s\n" "$direc"
-   cd $CURR_DIR
-
+   
 done
 
+cd $CURR_DIR
 
 exit 0
 
--- aws_hadoop_harness/parser.pl
+++ aws_hadoop_harness/parser.pl
@@ -1,32 +1,46 @@
 
-
 # See http://articles.techrepublic.com.com/5100-22-5363190.html
 #  for installation instruction and usage of XML::Simple;
 # More References for XML:Simple:
 #--------------------------------
 # http://www.ibm.com/developerworks/xml/library/x-xmlperl1.html#N100BC
 # http://www.tutorialized.com/view/tutorial/XML-Simple-Module/12239
-
-# use module
 use XML::Simple;
 use Data::Dumper;
 
-# Name of the XML configuration file produced for each experiment (in its own directory)
-my $XML_CONF_FILE_NAME = "configuration.xml";
+#####################################################################################
+
+# the parameter types that can appear in the input XML configuration file 
+my $MAPREDUCE_JOBCONF_TYPE = "MAPREDUCE_JOBCONF"; # for the configuration parameters
+my $HADOOP_JAR_TYPE = "HADOOP_JAR"; # for generating the hadoop jar command 
+my $TASKTRACKER_SETUP_TYPE = "TASKTRACKER_SETUP"; # tasktracker specific parameters
 
 # The default delimitter that separates multiple values in the 
 #    XML configuration file specified as input. This field can be overridden
 #    for any parameter by specifying a value_delimitter field 
 my $DEFAULT_VALUE_DELIMITTER = ",";
 
+# Name of the XML configuration file produced for each experiment (in its own directory)
+#  that specifies the parameter configuration for the MapReduce job that will 
+# run the experiment.
+my $XML_CONF_FILE_NAME = "configuration.xml";
+
+# Name of the hadoop command script produced for each experiment (in its own directory)
+my $HADOOP_JAR_COMMAND_SCRIPT = "run_hadoop_jar.sh";
+
+# The output of the hadoop command is written to this file (in each experiment directory)
+my $EXPERIMENT_OUTPUT_FILE = "output.txt";
+
+#####################################################################################
+
 # if #arguments is incorrect, print usage
 my ($numargs) = $#ARGV + 1;
 
 if ($numargs != 3) {
     #$^X is the name of this executable
-    print "USAGE: " . $^X . " <name of XML config file> <base directory for experiments> <output file containing list of experiment directories>\n";
-    
-    exit 0;
+    print "USAGE: " . $^X . " " . $0 . " <name of XML config file> <base directory for experiments> <output file containing list of experiment directories>\n";
+    print "NOTE: For $0 to run, the base directory should not exist. It will be created by the program.\n";
+    exit -1;
 }
 
 # create object
@@ -36,23 +50,41 @@ my $data = $xml->XMLin($ARGV[0]);
 
 my $EXP_BASE_DIR = $ARGV[1];
 
-unless (-d $EXP_BASE_DIR) {
-    print "ERROR: invalid base directory for experiments\n";
-    print "Exiting\n";
-    exit 0;    
+if (-d $EXP_BASE_DIR) {
+    print "ERROR: $EXP_BASE_DIR already exists. Please specify a different base directory for experiments. Exiting.\n";
+    print "NOTE: For $0 to run, the base directory should not exist. It will be created by the program.\n";
+    print "USAGE: " . $^X . " " . $0 . " <name of XML config file> <base directory for experiments> <output file containing list of experiment directories>\n";
+    exit -1;    
 }
 
-# dereference hash ref
-# access <property> array
-my @values = ();
-my $num_vals = -1;
+########################################################################
+
+# Global variables -- begin 
+
+# NUM_VALUES_ARR will contain the numvalues (i.e., <numvalues>2</numvalues>)
+#   specified for each parameter 
+my @NUM_VALUES_ARR = ();
+# TYPES_ARR will contain the type (i.e., <type>HADOOP_JAR</type>)
+#   specified for each parameter. Note that the default type is: $MAPREDUCE_JOBCONF_TYPE
+my @TYPES_ARR = ();
+# VALUES_DELIMITTER_ARR will contain the value delimitter 
+#   (i.e., <value_delimitter>|</value_delimitter>) specified for each parameter. 
+#   Note that $DEFAULT_VALUE_DELIMITTER = ",";
+my @VALUES_DELIMITTER_ARR = ();
 
 # number of parameters 
 my $num_params = 0;
-my $curr_pnum = 0;
 # total number of experiments
 my $total_expts = 1;
-my @NUM_VALUES_ARR = ();
+
+# Global variables -- end
+
+########################################################################
+
+# temporary variables used in the code below 
+my @values = ();
+my $num_vals = -1;
+my $curr_pnum = 0;
 my $value_delimitter;
 
 print "------------\n";
@@ -61,54 +93,57 @@ foreach $e (@{$data->{property}}) {
     
     $curr_pnum = $num_params + 1;
     print "Parameter $curr_pnum:\n";    
-    
     print "parameter name = ", $e->{name}, "\n";
-    print "number of values = ", $e->{numvalues}, "\n";
     
-    if ($e->{numvalues} <= 0) {
-	print "ERROR: Incorrect specification of numvalues tag for property " . $e->{name} . "\n";
-	print "Exiting\n";
-	exit 0;
+    # parameter type 
+    if (defined($e->{type})) {
+	$TYPES_ARR[$num_params] = $e->{type};
     }
-    
-    # number of distinct settings for this parameter
-    $NUM_VALUES_ARR[$num_params] = $e->{numvalues};
-    
-    $num_params += 1;
-    # note (later on) we can have scripts that do not set any parameter
-    if ($e->{numvalues} > 0) {
-	$total_expts *= $e->{numvalues};
+    else {
+	$TYPES_ARR[$num_params] = $MAPREDUCE_JOBCONF_TYPE;
+	print "NOTE: parameter type was unspecified, so using the default type of $MAPREDUCE_JOBCONF_TYPE\n";
+    }
+    print "parameter type = $TYPES_ARR[$num_params]\n";	
+
+    print "number of values = ", $e->{numvalues}, "\n";
+    if ($e->{numvalues} <= 0) {
+	print "ERROR: numvalues for property " . $e->{name} . " should be > 0. Exiting\n";
+	exit -1;
     }
 
+    # number of distinct settings for this parameter
+    $NUM_VALUES_ARR[$num_params] = $e->{numvalues};
+
+    # value delimitter 
     if (!defined($e->{value_delimitter}) || $e->{value_delimitter} eq "") {
-	$value_delimitter = $DEFAULT_VALUE_DELIMITTER;
+	$VALUES_DELIMITTER_ARR[$num_params] = $DEFAULT_VALUE_DELIMITTER;
     }
     else {
 	if ($e->{value_delimitter} eq "|") {
 	    # We have to add an escape character to use "|" as a delimitter in split
-	    $value_delimitter = "\\|";
+	    $VALUES_DELIMITTER_ARR[$num_params] = "\\|";
 	}
 	else {
-	    $value_delimitter = $e->{value_delimitter};
+	    $VALUES_DELIMITTER_ARR[$num_params] = $e->{value_delimitter};
 	}
     }
     
-    print "value delimitter = $value_delimitter\n";
+    print "value delimitter = $VALUES_DELIMITTER_ARR[$num_params]\n";
     
     $num_vals = 0;
-    
     unless (!defined($e->{values}) || $e->{values} eq "") {
 	print $e->{values}, "\n";
-	@values = split (/$value_delimitter/, $e->{values});
+	@values = split (/$VALUES_DELIMITTER_ARR[$num_params]/, $e->{values});
 	$num_vals = $#values + 1;
     }
-
     
     unless ($num_vals == $e->{numvalues}) {
-	print "ERROR: Incorrect specification of numvalues tag for property " . $e->{name} . "\n";
-	print "Exiting\n";
-	exit 0;
+	print "ERROR: numvalues for property " . $e->{name} . " does not match the values specified. Exiting\n";
+	exit -1;
     }
+
+    $num_params += 1;
+    $total_expts *= $e->{numvalues};
 
     print "------------\n";
 }
@@ -116,7 +151,7 @@ foreach $e (@{$data->{property}}) {
 print "Number of parameters is $num_params\n";
 print "Total number of experiments is $total_expts\n";
 
-# this is the 2D array where we will store the parameter values for each 
+# This is the 2D array where we will store the parameter values for each 
 # experiment on a row-by-row basis. The columns correspond 1-to-1 to the 
 # parameters.
 my @ARR = ();
@@ -135,10 +170,6 @@ my $pnum = -1;
 foreach $e (@{$data->{property}}) {
     $pnum ++;
     
-    if ($e->{numvalues} == 0) {
-	next;
-    }
-    
     my $outer_prod = 1;    
     # note: pnum is actually a param index
     for ($j = 0; $j < $pnum; $j++) {    
@@ -154,20 +185,7 @@ foreach $e (@{$data->{property}}) {
 	}
     }    
     
-    if (!defined($e->{value_delimitter}) || $e->{value_delimitter} eq "") {
-	$value_delimitter = $DEFAULT_VALUE_DELIMITTER;
-    }
-    else {
-	if ($e->{value_delimitter} eq "|") {
-	    # We have to add an escape character to use "|" as a delimitter in split
-	    $value_delimitter = "\\|";
-	}
-	else {
-	    $value_delimitter = $e->{value_delimitter};
-	}
-    }
-    
-    @values = split (/$value_delimitter/, $e->{values});
+    @values = split (/$VALUES_DELIMITTER_ARR[$pnum]/, $e->{values});
     
     my $index = -1;
     my ($a, $b, $c);
@@ -195,9 +213,11 @@ print "***************************************************\n";
 
 
 # At the point, the list of experiments is ready. We now have to 
-#   create a directory for each experiment. The directory should contain 
-#   an XML configuration that will specify the corresponding parameter values
-#
+#   create a directory for each experiment. The directory should contain: 
+#   -- an XML configuration that will specify the corresponding parameter values
+#         (this XML file is named $XML_CONF_FILE_NAME = "configuration.xml")
+#   -- a script to run the corresponding Hadoop MapReduce job
+#         (this script is named $HADOOP_JAR_COMMAND_SCRIPT = "run_hadoop_jar.sh")
 
 my $CURR_DIR=`pwd`;
 chomp($CURR_DIR);
@@ -213,31 +233,98 @@ for ($i = 0; $i < $total_expts; $i++) {
     if (-d $dir_name) {
 	print "ERROR: Expt directory \"$dir_name\" already exists\n";
 	print "Exiting\n";
-	exit 0;    
+	exit -1;    
     }
     
     print "Making directory $dir_name\n";
-    mkdir $dir_name, 0755;
+    `mkdir -p $dir_name`;
+    `chmod 755 $dir_name`;
     
     chdir $dir_name;
     
     # array where we will store the property_name, value combinations to be written
-    #   out to the XML configuration file
+    #   out to the XML configuration file for each $MAPREDUCE_JOBCONF_TYPE parameter
     my @conf_array = ();
-    
-    # add the property_name, value combination from each experiment to the conf_array
+    $j = -1; # $j is used to access (only) conf_array 
     $pnum = -1;
+    my $jar_path = "";
+    my $jar_class_name = "";
+    my $jar_input_params = "";
+    my $hdfs_output_path = "";
+    my $delete_hdfs_output_on_exit = 1;
+    my $hadoop_run_jar_command = "";
+    my $hadoop_delete_hdfs_output_command = "";
+    
     foreach $e (@{$data->{property}}) {
 	$pnum ++;
-	$conf_array[$pnum] = {'name' => $e->{name}, 'value' => $ARR[$i][$pnum]};
+        if ($TYPES_ARR[$pnum] eq $MAPREDUCE_JOBCONF_TYPE) {
+	    $j ++;
+	    $conf_array[$j] = {'name' => $e->{name}, 'value' => $ARR[$i][$pnum]};
+	}
+        elsif ($TYPES_ARR[$pnum] eq $HADOOP_JAR_TYPE) {
+	    if ($e->{name} eq "jar_path") {
+		$jar_path = $ARR[$i][$pnum];
+	    }
+	    elsif ($e->{name} eq "jar_class_name") {
+		$jar_class_name = $ARR[$i][$pnum];
+	    }
+	    elsif ($e->{name} eq "jar_input_params") {
+		$jar_input_params = $ARR[$i][$pnum];
+	    }
+	    elsif ($e->{name} eq "jar_output_prefix") {
+		# The output directory has the form ${jar_output_prefix}/${exptID},
+		# where ${exptID} is an identifier for the experiment. For example, 
+		# if we specified jar_output_prefix as /user/shivnath/tera/out,
+		# then the output directory for the Experiment 1 will be
+		# /user/shivnath/tera/out/expt1
+		$hdfs_output_path = $ARR[$i][$pnum] . "/expt" . "$i";
+		
+		# should the hdfs output be deleted when the experiment completes
+		if (defined($e->{delete_on_exit}) && ($e->{delete_on_exit} eq "false")) {
+		    $delete_hdfs_output_on_exit = 0;
+		}
+	    }
+	    else {
+		print "ERROR: parameter " . $e->{name} . " of type $HADOOP_JAR_TYPE is not supported. Exiting\n";
+		exit -1;		
+	    }
+	}
+	elsif ($TYPES_ARR[$pnum] eq $TASKTRACKER_SETUP_TYPE) {
+	    print "ERROR: parameter " . $e->{name} . " has type $TYPES_ARR[$pnum] which is not supported yet. Exiting\n";
+	    exit -1;
+	}
+	else {
+	    print "ERROR: parameter " . $e->{name} . " has unsupported type $TYPES_ARR[$pnum]. Exiting\n";
+	    exit -1;
+	}
     } # for pnum 
     
-    # create object
+    # create the $HADOOP_JAR_COMMAND_SCRIPT
+    
+    # command to run the jar. Note: this command did not work when the -conf option
+    #    was put after the input params and the output path 
+    $hadoop_run_jar_command = '${HADOOP_HOME}' . "/bin/hadoop jar $jar_path $jar_class_name -conf $XML_CONF_FILE_NAME $jar_input_params $hdfs_output_path" . " >& $EXPERIMENT_OUTPUT_FILE &";
+    $hadoop_delete_hdfs_output_command = '${HADOOP_HOME}' . "/bin/hadoop fs -rmr $hdfs_output_path";
+    `echo '#!/usr/bin/env bash' >$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo 'printf "Going to run the command: %s\\n" "$hadoop_delete_hdfs_output_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo '$hadoop_delete_hdfs_output_command' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo 'printf "Finished running the command: %s\\n" "$hadoop_delete_hdfs_output_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo 'printf "Going to run the command: %s\\n" "$hadoop_run_jar_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo '$hadoop_run_jar_command' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    `echo 'wait \$!' >>$HADOOP_JAR_COMMAND_SCRIPT`; 
+    `echo 'printf "Finished running the command: %s\\n" "$hadoop_run_jar_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    if ($delete_hdfs_output_on_exit == 1) {
+	`echo 'printf "Going to run the command: %s\\n" "$hadoop_delete_hdfs_output_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+	`echo '$hadoop_delete_hdfs_output_command' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+	`echo 'printf "Finished running the command: %s\\n" "$hadoop_delete_hdfs_output_command"' >>$HADOOP_JAR_COMMAND_SCRIPT`;
+    }
+    `chmod 544 $HADOOP_JAR_COMMAND_SCRIPT`; 
+    
+    # create the $XML_CONF_FILE_NAME
     $xml = new XML::Simple (NoAttr => 1, 
 			    RootName => 'configuration', 
 			    OutputFile => $XML_CONF_FILE_NAME,
 			    XMLDecl    => "<?xml version='1.0'?>");
-    
     # convert Perl array ref into XML document 
     $xml->XMLout({'property' => \@conf_array});
     
