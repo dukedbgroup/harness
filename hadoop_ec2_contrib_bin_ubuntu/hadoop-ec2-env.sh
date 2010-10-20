83e879f4225b2d0ec3d31dbc5cbd68ae89355c46
Harold Lim <harold@cs.duke.edu>
2010-10-19 20:45:04 -0400

    New harness scripts for hadoop 0.21 in ubuntu image

---
+++ hadoop_ec2_contrib_bin_ubuntu/hadoop-ec2-env.sh
@@ -0,0 +1,160 @@
+# Set environment variables for running Hadoop on Amazon EC2 here. All are required.
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Your Amazon Account Number.
+AWS_ACCOUNT_ID=${AWS_USER_ID}
+
+# Your Amazon AWS access key. 
+#  Should already be defined as a global environmental variable 
+#     (e.g., in ${HOME}/.my-bash_profile) if you followed 
+#     the instructions in the README. Hence commented out.
+#AWS_ACCESS_KEY_ID=
+
+# Your Amazon AWS secret access key. 
+#  Should already be defined as a global environmental variable 
+#     (e.g., in ${HOME}/.my-bash_profile) if you followed 
+#     the instructions in the README. Hence commented out.
+#AWS_SECRET_ACCESS_KEY=
+
+# Location of EC2 keys.
+#  $EC2_PRIVATE_KEY should be defined as a global environmental variable 
+#     (e.g., in ${HOME}/.my-bash_profile) if you followed 
+#     the instructions in the README
+EC2_KEYDIR=`dirname "$EC2_PRIVATE_KEY"`
+
+# The EC2 key name used to launch instances. Change it as needed. 
+KEY_NAME=my-keypair
+# Ned's convention:
+#KEY_NAME="${EC2_KEYPAIR_NAME}"
+
+# Where your EC2 private key is stored (created, for example, when following the 
+#    Amazon Getting Started guide).
+PRIVATE_KEY_PATH=`echo "${EC2_KEYDIR}"/"id_rsa-${KEY_NAME}"`
+# Ned's convention:
+#PRIVATE_KEY_PATH=`echo "$EC2_KEYDIR"/"$KEY_NAME"`
+
+# The EC2 instance type: m1.small, m1.large, m1.xlarge
+#  NOTE: we do not support AMIs for all types of instances
+#INSTANCE_TYPE="m1.small"
+INSTANCE_TYPE="m1.large"
+#INSTANCE_TYPE="m1.xlarge"
+#INSTANCE_TYPE="c1.medium"
+#INSTANCE_TYPE="c1.xlarge"
+
+# The version of Hadoop to use.
+#  Note: HADOOP_VERSION has to be 0.19.0 or less, or 0.20.2. AMIs can be accessed 
+#    for these versions only. Intermediate versions are not supported. 
+#    See launch-hadoop-master and launch-hadoop-slaves for how the AMI is 
+#      selected based on HADOOP_VERSION
+#HADOOP_VERSION=0.19.0
+HADOOP_VERSION=0.20.2
+
+
+# Import local settings if they exists and OVEWRITE the defaults
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+if [[ -f "$bin"/local_ec2_settings.sh ]] 
+then
+  . "$bin"/local_ec2_settings.sh 
+fi
+
+###################################################################################
+###################################################################################
+###################################################################################
+#
+# Variables below usually need not be changed
+#
+###################################################################################
+###################################################################################
+###################################################################################
+
+# SSH options used when connecting to EC2 instances.
+SSH_OPTS=`echo -i "$PRIVATE_KEY_PATH" -o StrictHostKeyChecking=no -o ServerAliveInterval=30`
+
+# The script to run on instance boot.
+if [ $HADOOP_VERSION = "0.20.2" ]; then
+  USER_DATA_FILE=hadoop-ec2-init-remote-post-0.20.0.sh
+else 
+  USER_DATA_FILE=hadoop-ec2-init-remote-pre-0.20.0.sh
+fi
+
+# The Amazon S3 bucket where the Hadoop AMI is stored. Used only for HADOOP_VERSION <= 0.19.0 
+# The default value is for public images, so can be left if you are using running a public image.
+# Change this value only if you are creating your own (private) AMI
+# so you can store it in a bucket you own.
+S3_BUCKET=hadoop-images
+
+# Enable public access to JobTracker and TaskTracker web interfaces
+ENABLE_WEB_PORTS=true
+
+# The EC2 group master name. CLUSTER is set by calling scripts
+CLUSTER_MASTER=$CLUSTER-master
+
+# Cached values for a given cluster
+MASTER_PRIVATE_IP_PATH=~/.hadooop-private-$CLUSTER_MASTER
+MASTER_IP_PATH=~/.hadooop-$CLUSTER_MASTER
+MASTER_ZONE_PATH=~/.hadooop-zone-$CLUSTER_MASTER
+
+######################################################################
+#
+# The following variables are primarily used when creating an AMI.
+#
+
+# The version number of the installed JDK.
+JAVA_VERSION=1.6.0_20
+
+# SUPPORTED_ARCHITECTURES = ['i386', 'x86_64']
+# The download URL for the Sun JDK. Visit http://java.sun.com/javase/downloads/index.jsp and get the URL for the "Linux self-extracting file".
+if [ "$INSTANCE_TYPE" == "m1.small" -o "$INSTANCE_TYPE" == "c1.medium" ]; then
+  ARCH='i386'
+  BASE_AMI_IMAGE="ami-2b5fba42"  # ec2-public-images/fedora-8-i386-base-v1.07.manifest.xml
+  JAVA_BINARY_URL=''
+else
+  ARCH='x86_64'
+  BASE_AMI_IMAGE="ami-2a5fba43"  # ec2-public-images/fedora-8-x86_64-base-v1.07.manifest.xml
+  JAVA_BINARY_URL=''
+fi
+
+if [ "$INSTANCE_TYPE" == "c1.medium" ]; then
+  AMI_KERNEL=aki-9b00e5f2 # ec2-public-images/vmlinuz-2.6.18-xenU-ec2-v1.0.i386.aki.manifest.xml
+fi
+
+if [ "$INSTANCE_TYPE" == "c1.xlarge" ]; then
+  AMI_KERNEL=aki-9800e5f1 # ec2-public-images/vmlinuz-2.6.18-xenU-ec2-v1.0.x86_64.aki.manifest.xml
+fi
+
+if [ "$AMI_KERNEL" != "" ]; then
+  KERNEL_ARG="--kernel ${AMI_KERNEL}"
+fi
+
+######################################################################
+
+# Finding Hadoop image. See https://wiki.duke.edu/display/hadoop/List+of+Current+AMI+Images
+# AMI ami-5729c03e: Ned created with Hadoop 0.20.2
+# AMI ami-0153ba68: Shivnath created with Hadoop 0.20.2 and Ganglia (DEPRECATED)
+# AMI ami-2817ff41: Shivnath created with Hadoop 0.20.2, Ganglia, and BTrace
+if [ $HADOOP_VERSION = "0.20.2" ]; then
+   # AMI_IMAGE="ami-5729c03e"
+   # AMI_IMAGE="ami-0153ba68"
+   #AMI_IMAGE="ami-2817ff41"
+   AMI_IMAGE="ami-f617e39f"
+else 
+   # This part will only work for $HADOOP_VERSION being 0.19.0 or less 
+   AMI_IMAGE=`ec2-describe-images -a | grep $S3_BUCKET | grep $HADOOP_VERSION | grep $ARCH | grep available | awk '{print $2}'`   
+fi
+
+######################################################################
---
+++ hadoop_ec2_contrib_bin_ubuntu/start_hadoop_on_slaves.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# NOTE: This file will be copied to and run on the EC2 Hadoop Master node
+#         to start the Hadoop slave nodes
+
+# These two files will be scp'ed in launch-hadoop-slaves
+#    as part of the starting the slaves
+HOSTLIST="/home/ubuntu/SLAVE_NAMES.txt"
+EC2_INIT_SCRIPT="/home/ubuntu/ec2_hadoop_slave_init.sh"
+
+# SSH options used when connecting to EC2 Slave instances from the Master
+SSH_OPTS=`echo -o StrictHostKeyChecking=no -o ServerAliveInterval=30`
+
+# some slack to ensure that the ssh service starts working on the
+#     slave nodes before the master attempts to ssh into them
+declare -i NUM_SSH_ATTEMPTS=4
+# wait interval between successive ssh attempts
+declare -i SSH_WAIT_INTERVAL=5 
+
+for slave_private_ip in `cat "$HOSTLIST"`; do
+ {
+
+  # each instance of the for loop is run in a subshell -- grouped using "(" and ")"
+  (
+
+   # if we fail to start the slave automatically, then a manual start will be required
+   declare -i MANUAL_START_REQUIRED=1
+   declare -i attempt_num=0
+   declare REPLY
+
+   # try ssh to see whether we can connect to the slave 
+   for (( attempt_num=0; attempt_num < $NUM_SSH_ATTEMPTS; attempt_num++ )) ; do
+     REPLY=`ssh $SSH_OPTS "ubuntu@${slave_private_ip}" 'echo "hello"'`
+     if [ ! -z $REPLY ]; then
+        # ssh worked 
+        MANUAL_START_REQUIRED=0
+        break;
+     fi
+     sleep $SSH_WAIT_INTERVAL
+   done
+
+   if [ $MANUAL_START_REQUIRED = 0 ]; then
+        # scp and automatic start should work now for this slave
+       scp $SSH_OPTS $EC2_INIT_SCRIPT "ubuntu@${slave_private_ip}:${EC2_INIT_SCRIPT}"
+       ssh $SSH_OPTS "ubuntu@${slave_private_ip}" "sudo ${EC2_INIT_SCRIPT}"
+   else 
+       echo "ERROR: Automatic start of Hadoop on slave node ${slave_private_ip} failed. Manual start required."
+   fi 
+ 
+  ) &
+   
+ } 
+done
+
+wait
+
+  echo ""
+  echo "*********************************************************************"
+  echo "NOTE: On JobTracker web page, verify that all slave nodes have joined"
+  echo "      the cluster (it may take a few seconds for all slaves to join)"
+  echo "      JobTracker web page is at: http://<HADOOP_MASTER_NODE>:50030"
+  echo ""
+  echo "NOTE: If automatic start failed for any slave, login to the Hadoop"
+  echo "      master node on EC2 and run /home/ubuntu/start_hadoop_on_slaves.sh"
+  echo "*********************************************************************"
+  echo ""
